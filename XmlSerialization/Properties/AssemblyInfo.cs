fa42573d966a12ce0985c728a5eca4dc48904c43
abazyan <arsen.abazyan@devexpress.com>
2022-07-26 18:21:41 +0300

    Add RecentItemsList

--- XmlSerialization/Properties/AssemblyInfo.cs
+++ XmlSerialization/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XmlSerialization")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Xml Serialization Helper")]
+[assembly: AssemblyDescription("This helper is created to simplify work with XmlSerializer class.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Arsen Abazian")]
 [assembly: AssemblyProduct("XmlSerialization")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Not used by NuGet")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
